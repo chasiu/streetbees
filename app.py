3d3e709c4e03fb2a4c0cfa243ab195dea9bd9ce0
Kristian McCaul <kristian.mccaul@outlook.com>
2021-02-23 22:05:52 +0000

    Added code to call model and return prediction to user

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, request, redirect
+from joblib import load
 
 app = Flask(__name__)
+
+clf = load("models/base_model.joblib")
+vectorizer = load("models/vectorizer.joblib")
+f = open("data/classes.txt", "r")
+classes = {}
+c = 1
+for i in f:
+    classes[c] = i.strip("\n")
+    c += 1
 
 
 @app.route("/")
 def data_entry():
-    return render_template("index.html")
+    pred = ""
+    return render_template("index.html", pred=pred)
 
 
 @app.route("/identify", methods=["POST"])
 def identify():
-    data = request.form["data"]
-
-    return redirect("/")
+    string = [request.form["data"]]
+    # TODO Cleaning script
+    vec = vectorizer.transform(string)
+    pred = clf.predict(vec)[0]
+    result = "Predicted class: {}".format(classes[pred])
+    return render_template("index.html", pred=result)
 
 
 if __name__ == "__main__":
