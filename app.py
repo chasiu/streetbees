4e8bf688968b4df0b2effe97d5dc16c2b9fa3035
Kristian McCaul <kristian.mccaul@outlook.com>
2021-02-24 16:48:50 +0000

    Added mongodb to store the text & prediction of the model. Added a separate API route

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from joblib import load
+import pymongo
+from config import DB_URI, CLASSES
+
+client = pymongo.MongoClient(DB_URI)
+db = client["streetbees"]
+logs = db.logs
+
 
 app = Flask(__name__)
 
 clf = load("models/base_model.joblib")
 vectorizer = load("models/vectorizer.joblib")
-f = open("data/classes.txt", "r")
-classes = {}
-c = 1
-for i in f:
-    classes[c] = i.strip("\n")
-    c += 1
+classes = CLASSES
 
 
 @app.route("/")
@@ -19,14 +21,31 @@ def data_entry():
     return render_template("index.html", pred=pred)
 
 
-@app.route("/identify", methods=["POST"])
+@app.route("/results", methods=["POST"])
 def identify():
     string = [request.form["data"]]
     # TODO Cleaning script
     vec = vectorizer.transform(string)
     pred = clf.predict(vec)[0]
+    text = "Name entered: {}".format(string[0])
     result = "Predicted class: {}".format(classes[pred])
-    return render_template("index.html", pred=result)
+    log = {"text": str(string), "predicted_class": int(pred)}
+    logs.insert_one(log).inserted_id
+    return render_template("index.html", pred=result, text=text)
+
+
+@app.route("/api/v1/names/classify", methods=["GET"])
+def classify_api():
+    if "name" in request.args:
+        name = str(request.args["name"])
+    else:
+        return "Error. Please no name field provied. Please provide a name."
+
+    vec = vectorizer.transform([name])
+    pred = clf.predict(vec)[0]
+    result = {name: classes[pred]}
+
+    return jsonify(result)
 
 
 if __name__ == "__main__":
